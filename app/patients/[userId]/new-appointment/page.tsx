072a79936456205d92e341cfb1bacfa0936d37a8
Hafiz N <hafiznjh007@gmail.com>
2024-07-19 16:12:02 +0530

    feat: integrate schedule appointment page

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,9 +1,42 @@
-const NewAppointment = () => {
+import Image from "next/image";
+
+import AppointmentForm from "@/components/froms/appointment-form";
+import { getPatient } from "@/lib/actions/patient.actions";
+
+export default async function NewAppointment({
+  params: { userId },
+}: SearchParamProps) {
+  const patient = await getPatient(userId);
+
   return (
-    <div>
-      <h1>New Apppointment Page</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="logo"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <AppointmentForm
+            patientId={patient?.$id}
+            userId={userId}
+            type="create"
+          />
+
+          <p className="copyright mt-10 py-12">{`© ${new Date().getFullYear()} CareBook`}</p>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1500}
+        width={1500}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
     </div>
   );
-};
-
-export default NewAppointment;
+}
